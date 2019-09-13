27d64c823567f9dbffcd3603703c6e59376be8ce
Bingyy <bing@bingw.me>
2019-09-14 00:19:08 +0800

    update README

---
+++ frontend/src/components/CarRegister.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Basic Car Info Form</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "CarRegister",
+    
+}
+</script>
+
+<style scoped>
+    
+</style>
---
+++ frontend/src/components/SmartCarDashboard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>
+            Smart Car Dashboard
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SmartCarDashboard",
+    props: {
+    },
+    
+}
+</script>
+
+<style>
+    
+</style>
